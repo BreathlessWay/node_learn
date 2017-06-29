d13195497c350f99fa56ce6796c834382ff72da4
朱健 <zhujian@yowits.com>
2017-06-29 17:26:45 +0800

    no message

--- server.js
+++ server.js
@@ -3,11 +3,29 @@
  */
 const app = require('express')();
 const utility = require('utility');
+const superagent = require('superagent');
+const cheerio = require('cheerio');
 app.get('/', (request, response) => {
-  console.log(request.headers);
-  const q = request.query.q;
-  const qMd5 = utility.md5(q);
-  response.send(qMd5);
+  // console.log(request.headers);
+  // const q = request.query.q;
+  // const qMd5 = utility.md5(q);
+  // response.send(qMd5);
+  superagent.get('https://cnodejs.org/')
+      .end((err, res) => {
+        if (err) {
+          return next(err);
+        }
+        const $ = cheerio.load(res.text);
+        const content = [];
+        $('#topic_list .topic_title').each((index, item) => {
+          const json = {
+            title: $(item).attr('title'),
+            href: $(item).attr('href')
+          };
+          content.push(json);
+        });
+        response.send(content);
+      });
 });
 app.listen(8080, () => {
   console.log('server is running on http://localhost:8080');
