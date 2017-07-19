1095166eff42a33eae4f0d4256d5233255fdfc66
BreathlessWay <731005087@qq.com>
2017-07-19 22:44:09 +0800

    no message

--- runoob/runoob-node.js
+++ runoob/runoob-node.js
@@ -1,20 +1,34 @@
 /**
  * Created by Admin on 2017/7/14.
  */
-const os = require('os');
-const path = require('path');
-const dns = require('dns');
-console.log(path.resolve('', 'index.js'));
+// const os = require('os');
+// const path = require('path');
+// const dns = require('dns');
+// console.log(path.resolve('', 'index.js'));
 
-console.log(path.join('views', 'index.js'));
+// console.log(path.join('views', 'index.js'));
 
-dns.lookup('sina.cn', function onLookup(err, address, family) {
-  console.log('ip 地址:', address);
-  dns.reverse(address, function (err, hostnames) {
-    if (err) {
-      console.log(err.code);
-    }
+// dns.lookup('sina.cn', function onLookup(err, address, family) {
+//   console.log('ip 地址:', address);
+//   dns.reverse(address, function (err, hostnames) {
+//     if (err) {
+//       console.log(err.code);
+//     }
 
-    console.log('反向解析 ' + address + ': ' + JSON.stringify(hostnames));
+//     console.log('反向解析 ' + address + ': ' + JSON.stringify(hostnames));
+//   });
+// });
+
+const fs = require('fs');
+
+fs.readdir('C:/Users/Administrator/Desktop/Game', (err, files) => {
+  if (err) throw err;
+  const fileArray = [];
+  files.forEach((list, index) => {
+    const json = {};
+    json.id = `img${index}`;
+    json.src = `C:/Users/Administrator/Desktop/Game/${list}`;
+    fileArray.push(JSON.stringify(json));
   });
+  fs.appendFile('./fileList.json', fileArray);
 });
