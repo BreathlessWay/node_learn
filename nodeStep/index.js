6cd353ade117971d446071beb8042595334cc57c
BreathlessWay <731005087@qq.com>
2017-08-25 16:38:37 +0800

    no message

--- nodeStep/index.js
+++ nodeStep/index.js
@@ -4,4 +4,13 @@
  * @date    2017-08-25 10:17:41
  * @version $Id$
  */
+const EventEmitter = require('events').EventEmitter;
+const event = new EventEmitter();
 
+event.on('nopa', (...arg) => {
+    console.log(...arg)
+})
+
+event.emit('nopa')
+
+console.log(event.listeners('nopa').toString())
