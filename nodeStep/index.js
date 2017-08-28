0e4d49d76444cff3c7be361cf2521aafd9210400
BreathlessWay <731005087@qq.com>
2017-08-28 17:06:24 +0800

    no message

--- nodeStep/index.js
+++ nodeStep/index.js
@@ -6,11 +6,23 @@
  */
 const EventEmitter = require('events').EventEmitter;
 const event = new EventEmitter();
+const crypto = require('crypto');
+const schedule = require('node-schedule');
 
 event.on('nopa', (...arg) => {
     console.log(...arg)
 })
 
+const pwd = '1234567'
+const md5 = crypto.createHash('md5')
+const _pwd = en_upwd = md5.update(pwd).digest('hex');
+
+console.log(_pwd)
+
 event.emit('nopa')
 
 console.log(event.listeners('nopa').toString())
+
+schedule.scheduleJob('30 * * * * *', function() {
+    console.log('schedule' + new Date())
+})
