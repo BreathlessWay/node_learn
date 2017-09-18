7cd1070b376c474c0914f259c8240bdcfe0512c9
BreathlessWay <731005087@qq.com>
2017-09-18 21:47:08 +0800

    ts入门

--- typescript/index.js
+++ typescript/index.js
@@ -1,5 +1,33 @@
-function greeter(person) {
-    return "Hello, " + person;
+function temp(template, name, cb) {
+    console.log({ template: template });
+    console.log(name);
+    console.log(cb());
 }
-var user = '';
-document.body.innerHTML = greeter(user);
+var a = 'fff';
+var c = 0;
+var b = function () {
+    return 15;
+};
+(_a = ["i am fuck ", " ,but ", ""], _a.raw = ["i am fuck ", " ,but ", ""], temp(_a, a, b));
+a = 13;
+c = 'f';
+function mm(name) {
+    return "gg";
+}
+mm('1');
+var Person = (function () {
+    function Person() {
+    }
+    return Person;
+}());
+var person = new Person();
+console.log(person.name = 1);
+console.log(person.age = 1);
+function mgg(a, b, c) {
+    if (c === void 0) { c = 'f'; }
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+mgg('fff');
+var _a;
