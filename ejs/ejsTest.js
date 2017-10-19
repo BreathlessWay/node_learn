8b027575ca1171d1fd9982e8d61d120ec0005a09
BreathlessWay <731005087@qq.com>
2017-10-19 14:24:23 +0800

    res.sendFile(path.join(__dirname,'index.html'))

--- ejs/ejsTest.js
+++ ejs/ejsTest.js
@@ -18,40 +18,44 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(methodOverride());
 app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: 'uwotm8'
+    resave: true,
+    saveUninitialized: true,
+    secret: 'uwotm8'
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('*', (req, res, next) => {
-  console.log('all');
-  console.log(req.app.get('port'));
-  next();
+    console.log('all');
+    console.log(req.app.get('port'));
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.use('/user', useRouter);
 
 app.post('/upload', upload.array('file', 50), (request, response) => {
-  console.log(response);
+    console.log(response);
 });
 
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  // next(err);
-  res.render('404', {
-    err
-  });
+app.use(function (req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    // next(err);
+    res.render('404', {
+        err
+    });
 });
 
 // 错误处理中间件应当在路由加载之后才能加载
 if ('development' === app.get('env')) {
-  app.use(errorHandler());
+    app.use(errorHandler());
 }
 
 app.listen(app.get('port'), () => {
-  console.log(`server is running on http://localhost:${app.get('port')}`);
+    console.log(`server is running on http://localhost:${app.get('port')}`);
 });
